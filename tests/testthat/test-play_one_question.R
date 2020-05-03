780b6d50a49ca371478f52553ac0a5f4d426f810
jennalandy <jennalandy0@gmail.com>
2020-05-03 15:56:03 -0700

    updating description and testing category_search

--- tests/testthat/test-play_one_question.R
+++
@@ -1,9 +0,0 @@
-# test_that("returns data frame", {
-#   questions_info <- get_board(6)
-#   questions <- questions_info$questions
-#   category_ids <- questions_info$category_ids
-#
-#   questions <- play_one_question(questions, category_ids)
-#
-#   expect_equal(class(questions), 'data.frame')
-# })
